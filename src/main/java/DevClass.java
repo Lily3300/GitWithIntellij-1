0f54a965deb88238074d16a974065806b23fd4f2
Lily3300 <96707020+Lily3300@users.noreply.github.com>
2021-12-26 18:41:56 -0800

    Update DevClass.java
    why there in no change

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -2,5 +2,6 @@ public class DevClass {
     public static void main(String[] args) {
         System.out.println("DEv Class + added");
         //checkin from strea2
+        //doing more changes
     }
 }
